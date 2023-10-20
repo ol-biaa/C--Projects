5aa88d7be2935ff6e54d3eee21f23c2353a24c38
ol-biaa <olbiaaletta@gmail.com>
2023-10-21 00:00:45 +0300

    game

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+string board[9]= {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+int player = 1;
+int position = 0;
+
+void introduction(){ 
+    cout << "Press ENTER to begin";
+    getchar()//   
+}
 int main(){
-    
+ 
 
 
 
